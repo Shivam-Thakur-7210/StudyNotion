4f4b3212439b714007549e87fac789d45c190643
Shivam-Thakur-7210 <thakurshivam7210@gmail.com>
2026-09-13 19:46:36 +0530

    payment controller name changed

--- megaProjectBackend/routes/Payments.js
+++ megaProjectBackend/routes/Payments.js
@@ -1,8 +1,8 @@
-// Import the required modules
+
 const express = require("express")
 const router = express.Router()
 
-const { capturePayment, verifySignature,sendPaymentSuccessEmail } = require("../controllers/Payment")
+const { capturePayment, verifySignature,sendPaymentSuccessEmail } = require("../controllers/payment")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment",auth,verifySignature)
